91303b579d69b6d912a64c21aad24c68cd4bb6e7
alok-07-pandey <alok00007pandey@gmail.com>
2025-04-28 10:23:59 +0530

    Video CV Added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -60,7 +60,22 @@ const About = () => {
               boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
             }}
           >
-            DOWNLOAD CV
+            DOWNLOAD CV 
+          </a>
+
+         { /*  Video CV */}
+
+         <a
+            href="https://drive.google.com/file/d/12CTCPWDFHQNz-KfAj_5AdexqoYC6c0LZ/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+            }}
+          >
+            VIDEO CV
           </a>
           
         </div>
